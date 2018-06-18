25ca087c8dd6871e0fdb0ea4270c0fbdbd344e5b
IM-jie <1154762493@qq.com>
2018-06-19 00:46:48 +0800

    First commit.

---
+++ app/src/main/java/com/example/jie/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.jie.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String qlty;
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
